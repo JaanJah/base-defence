8fa2126053c8cf28162fced9160c935335f2b389
hindrekhan <hindrek.hannus@tptlive.ee>
2019-08-24 19:11:00 +0300

    Add enemy

--- main.lua
+++ main.lua
@@ -1,15 +1,20 @@
 function love.load()
 	Object = require "classic"
+	require "enemy"
+	require "enemies/orc"
 	require "player"
 
+	orc = Orc()
 	player = Player()
 end
 
 function love.update(dt)
 	player:update(dt)
+	orc:update(dt)
 end
 
 function love.draw()
 	player:draw()
+	orc:draw()
 end
 
